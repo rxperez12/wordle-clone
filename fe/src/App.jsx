ff98ef6857883447813a3428468830ab9c7e6d6a
Ricardo Perez <rxperez12@gmail.com>
2024-08-06 17:09:39 -0700

    Replace home template

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,40 +1,33 @@
-import { useState } from 'react';
-import logo from './logo.svg'
-import './App.css';
+import { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import NavBar from "./routes-nav/NavBar";
 
+import "./App.css";
 
 /** Component for entire page.
  *
  * Props: none
  * State: none
  *
-*/
+ */
 
 function App() {
   const [count, setCount] = useState(0);
 
   function incrCount() {
-    setCount(count => count + 1);
+    setCount((count) => count + 1);
   }
 
   return (
     <div className="App">
+      <BrowserRouter>
+        <NavBar />
+      </BrowserRouter>
       <main>
-        <img src={logo} className="App-logo" alt="Rithm" />
-        <h1>Rithm React Starter</h1>
-        <p>
-          <button
-              className="btn btn-primary"
-              onClick={incrCount}>
-            Clicked: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>src/App.jsx</code>
-        </p>
+        <h1>Wordle</h1>,
       </main>
     </div>
   );
-};
+}
 
 export default App;
