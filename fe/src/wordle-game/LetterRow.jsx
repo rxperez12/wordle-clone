17195d087fcdbaeb27f58c98c68d559d7b71fff6
Ricardo Perez <rxperez12@gmail.com>
2024-08-07 16:22:33 -0700

    Add LetterRow component

---
+++ fe/src/wordle-game/LetterRow.jsx
@@ -0,0 +1,16 @@
+/** AppComponent for summary
+ *
+ * Props:
+ * -
+ *
+ * State:
+ * -
+ *
+ * call list
+ */
+
+function LetterRow() {
+  return <div> Multiple row of letters</div>;
+}
+
+export default LetterRow;
