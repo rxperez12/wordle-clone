da1cf2d7b2c4ce1ffc0303eb838650e6351c15c4
Ricardo Perez <rxperez12@gmail.com>
2024-08-06 17:09:16 -0700

    Add NavBar template

---
+++ fe/src/routes-nav/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+/** AppComponent for summary
+ *
+ * Props:
+ * -
+ *
+ * State:
+ * -
+ *
+ * call list
+ */
+
+function NavBar() {
+  return <h3> Nav Bar goes here</h3>;
+}
+
+export default NavBar;
