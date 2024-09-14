165eeeb2fcb657db919b26fa75d99b5b4d16af2d
Ricardo Perez <rxperez12@gmail.com>
2024-09-13 17:16:00 -0700

    Add handleGuess function to check user guess input and display result

--- fe/src/wordle-game/WordleGame.jsx
+++ fe/src/wordle-game/WordleGame.jsx
@@ -1,33 +1,46 @@
-import LetterRow from "./LetterRow";
-
-const practiceGuess = [
-  {
-    letter: "a",
-    category: "found",
-  },
-  {
-    letter: "p",
-    category: "contain",
-  },
-];
+import LetterRow from "./GuessWord.jsx";
+import GuessEnterForm from "./GuessEnterForm";
+import { useState } from "react";
+import { compareWordandAnswer } from "../gameLogic.js";
+import GuessWordList from "./GuessWordList.jsx";
 
 const CURRENT_GUESSES = 5;
-/** AppComponent for summary
- *
- * Props:
- * -
+/** WordleGame component
  *
  * State:
- * -
+ * - guesses: array of guesses like:
+ *    [
+ *      [{letter: str, category: str}, ...],
+ *      [{letter: str, category: str}, ...],
+ *    ...]
  *
- * App -> WordleGame -> LetterRow
+ *    where each array represents user guess and contains objects for each letter
+ *    of guess with keys "letter" for string letter and "category" that is an empty
+ *    string if letter does not exits, 'correct' if letter in correct position,
+ *    or 'exists' if letter in word but not correct position.
+ *
+ * Props:
+ *
+ *
+ * App -> WordleGame -> LetterRow, GuessEnterForm
  */
 
-function WordleGame({ word }) {
-  const guesses = new Array(CURRENT_GUESSES);
+function WordleGame({ answer }) {
+  const [guesses, setGuesses] = useState([]);
+
+  /* Handles guess input from user and add guess to state*/
+  function handleGuess(word) {
+    //TODO: word validation
+    console.log("input word", word);
+    const guess = compareWordandAnswer(answer, word);
+    console.log("guess data structure", guess);
+    setGuesses([...guesses, guess]);
+  }
+
   return (
     <div className="WordleGame m-3">
-      <LetterRow guess={practiceGuess} />
+      <GuessWordList guesses={guesses} />
+      <GuessEnterForm handleGuess={handleGuess} />
     </div>
   );
 }
