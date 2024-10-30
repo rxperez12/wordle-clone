8aec8602109239bca22ec0f254b01bfe2c6a96e0
Ricardo Perez <rxperez12@gmail.com>
2024-10-30 14:22:07 -0700

    Implement react-query to handle API call

--- fe/src/wordle-game/WordleGame.jsx
+++ fe/src/wordle-game/WordleGame.jsx
@@ -1,9 +1,10 @@
-import LetterRow from "./GuessWord.jsx";
 import GuessEnterForm from "./GuessEnterForm";
-import { useState } from "react";
-import { compareWordandAnswer } from "../gameLogic.js";
+import { useState, useEffect } from "react";
+import { compareWordandAnswer, checkValidEntry } from "../gameLogic.js";
+import { useQuery } from "@tanstack/react-query";
 import GuessWordList from "./GuessWordList.jsx";
 
+const BASE_URL = "http://localhost:8000";
 const CURRENT_GUESSES = 5;
 /** WordleGame component
  *
@@ -27,20 +28,48 @@ const CURRENT_GUESSES = 5;
 
 function WordleGame({ answer }) {
   const [guesses, setGuesses] = useState([]);
+  const [word, setWord] = useState("");
+  const { isPending, error, data, isLoading } = useQuery({
+    queryFn: retrieveWord,
+    queryKey: ["word"],
+  });
+
+  /* Retrieve guess word from API*/
+  async function retrieveWord() {
+    const response = await fetch(`${BASE_URL}/word/`);
+    if (!response.ok) {
+      throw new Error("Network response was not ok.");
+    }
+    return response.json();
+  }
 
   /* Handles guess input from user and add guess to state*/
   function handleGuess(word) {
-    //TODO: word validation
-    console.log("input word", word);
+    console.debug("handleGuess:", word);
     const guess = compareWordandAnswer(answer, word);
     console.log("guess data structure", guess);
     setGuesses([...guesses, guess]);
+    console.debug(guesses);
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return (
+      <div>
+        <div>An error occured: {error.message}</div>
+        <div>Please try again.</div>
+      </div>
+    );
   }
 
   return (
     <div className="WordleGame m-3">
-      <GuessWordList guesses={guesses} />
+      <h1>Wordle</h1>
       <GuessEnterForm handleGuess={handleGuess} />
+      <GuessWordList guesses={guesses} />
     </div>
   );
 }
