fff5ef172965ad2bf95ae4ddf0c56f054759d19d
Ricardo Perez <rxperez12@gmail.com>
2024-11-14 21:18:48 -0800

    Remove unneeded tests

--- fe/src/App.test.jsx
+++ fe/src/App.test.jsx
@@ -1,23 +1,9 @@
 import { describe, test, it, expect } from "vitest";
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent } from "@testing-library/react";
 
-import App from './App.jsx';
+import App from "./App.jsx";
 
-
-test('renders counter button', function () {
-  render(<App />);
-  const buttonElement = screen.getByText(/Clicked: 0/i);
-  expect(buttonElement).toBeInTheDocument();
-});
-
-test('count increases on button click', function () {
-  render(<App />);
-  const buttonElement = screen.getByText(/Clicked: 0/i);
-  fireEvent.click(buttonElement);
-  expect(buttonElement).toHaveTextContent(/Clicked: 1/i);
-});
-
-test('matches snapshot', function () {
+test("matches snapshot", function () {
   const { container } = render(<App />);
   expect(container).toMatchSnapshot();
 });
