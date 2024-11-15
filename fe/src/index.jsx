a37643e4bac191acbc7c1277ee3e830168719c3a
Ricardo Perez <rxperez12@gmail.com>
2024-11-15 09:37:21 -0800

    Add ReactQueryDevtools

--- fe/src/index.jsx
+++ fe/src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import App from "./App.jsx";
 
@@ -13,5 +14,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <QueryClientProvider client={queryClient}>
     <App />
+    <ReactQueryDevtools />
   </QueryClientProvider>,
 );
