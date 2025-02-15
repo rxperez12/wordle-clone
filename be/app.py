3d9274b9b054f2fe8bdfb18248eb6ba9b29c0169
Ricardo Perez <rxperez12@gmail.com>
2025-02-14 16:51:29 -0800

    Add Wordle endpoints and CORS support

--- be/app.py
+++ be/app.py
@@ -1,13 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 import os
+import word_logic
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 
-@app.get('/words')
+@app.get('/wordle')
 def get_word():
     """Return wordle word to user"""
 
-    return 'hello'
+    return jsonify(word_logic.get_wordle_word())
+
+
+@app.get('/wordle/search')
+def check_word_exists():
+    """Return true or false if guess is a valid word"""
+
+    guess = request.args.get('guess')
+
+    return jsonify(word_logic.check_guess_word(guess))
